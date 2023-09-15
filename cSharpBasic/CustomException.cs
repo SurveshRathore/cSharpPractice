d4896024ae3bcb84c96bc7a6ecd887c6ec8bcabf
SurveshRathore <educatedevilboy@gmail.com>
2023-09-15 14:30:03 +0530

    [Survesh] Added: custom Exception

---
+++ cSharpBasic/CustomException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpBasic
+{
+    public class CustomException:Exception
+    {
+        /// <summary>
+        /// These are the custom  exception types
+        /// </summary>
+        public enum ExceptionType
+        {
+            NULL_MESSAGE,
+            EMPTY_MESSAGE,
+            NO_SUCH_CLASS,
+            NOT_FOUND,
+            NO_SUCH_METHOD,
+            NOT_SUPPORTED,
+            NOT_IMPLEMENTED,
+            INVALID_OPERATIONS,
+            INVALID_INPUT
+        }
+
+        public ExceptionType Type { get; set; }
+
+        public CustomException (ExceptionType type, string message) : base(message)
+        {
+            this.Type = type;
+        }
+
+        /// <summary>
+        /// initializing in constructor
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="Message"></param>
+        public CustomException(string? message) : base(message)
+        {
+        }
+    }
+}
---
+++ cSharpBasic/TestingCustomException.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpBasic
+{
+    public class TestingCustomException
+    {
+        public int divide (int numerator, int denominator)
+        {
+			
+				if(denominator == 0)
+				{
+					throw new CustomException("Denominator cannot be zero");
+                    // throw new Exception("Denominator cannot be zero");
+                }
+
+				return numerator / denominator;
+			
+        }
+
+        public void calculations(int numerator, int denominator)
+        {
+            try
+            {
+                int result = divide(numerator, denominator);
+                Console.WriteLine(result);
+            }
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Custom Exception: " + ex.Message);
+            //}
+            catch (CustomException ex)
+            {
+                //Console.WriteLine ("Custom Exception: " + ex.Message);
+                throw new CustomException(CustomException.ExceptionType.INVALID_INPUT, "This is an invalid input");
+            }
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("General Exception: " + ex.Message);
+            //}
+        }
+    }
+}
